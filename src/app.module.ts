8926bc881eb09fe3a82328b993810fa6cbc1cb09
Patrick Code <wendeel.admpar@gmail.com>
2021-10-07 15:01:01 -0400

    add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { GameModule } from './game/game.module';
 import { ProfileModule } from './profile/profile.module';
 import { GenreModule } from './genre/genre.module';
 import { UserModule } from './user/user.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [GameModule, ProfileModule, GenreModule, UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
