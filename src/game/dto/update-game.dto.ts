0acacd0633051e91f598f36c2425723fb0facc0d
wendeel.admpar@gmail.com <Patrick Code>
2021-10-20 19:47:18 -0400

    ajust update

--- src/game/dto/update-game.dto.ts
+++ src/game/dto/update-game.dto.ts
@@ -1,8 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { IsInt } from 'class-validator';
 import { CreateGameDto } from './create-game.dto';
 
-export class UpdateGameDto extends PartialType(CreateGameDto) {
-  @IsInt()
-  id: number;
-}
+export class UpdateGameDto extends PartialType(CreateGameDto) {}
