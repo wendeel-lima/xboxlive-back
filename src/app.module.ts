b0ece31023d80715dfc7eb76ec1ab9084289bde6
wendeel.admpar@gmail.com <Patrick Code>
2021-10-25 15:49:13 -0400

    tratamento de erro

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,19 @@ import { GameModule } from './game/game.module';
 import { ProfileModule } from './profile/profile.module';
 import { GenreModule } from './genre/genre.module';
 import { UserModule } from './user/user.module';
-import { PrismaService } from './prisma/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 @Module({
   imports: [GameModule, ProfileModule, GenreModule, UserModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { UnauthorizedInterceptor } from './interceptors/unauthorized.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -14,6 +15,7 @@ async function bootstrap() {
     }),
   );
 
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
